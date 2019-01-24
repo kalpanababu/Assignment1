684ba531b10265350dcd4ab8e2bf2b41684706ae
pallavi <pallavismmh@gmail.com>
2019-01-23 23:40:10 -0800

    pallavi

---
+++ Desktop/Pallavi/selenium-java/src/test/java/automation/Java.java
@@ -0,0 +1,18 @@
+package automation;
+
+
+	import org.testng.annotations.Test;
+
+	public class Java {
+		
+		@Test
+		public void test() {
+			
+			System.out.println(" test1");
+		}
+		
+
+	
+
+
+}
