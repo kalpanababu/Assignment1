dedd60a2d07a9264a4bb48401cf700a5f8d4726a
Pallavipallu4 <pallavismmh8@gmail.com>
2019-01-24 01:03:49 -0800

    java

---
+++ Desktop/Pallavi/selenium-java/src/test/java/automation/java.java
@@ -0,0 +1,19 @@
+package automation;
+
+
+	
+	import org.testng.annotations.Test;
+
+	public class java {
+		
+		@Test
+		public void test() {
+			
+			System.out.println(" test1");
+		}
+		
+
+	}
+
+
+
